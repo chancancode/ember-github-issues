15198a5bacd1df715115cee55db61f456cd25d27
Godfrey Chan <godfreykfc@gmail.com>
2014-05-14 13:44:55 -0700

    Step 3: User info

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,14 @@
+var GHAPI = 'https://api.github.com';
+
+$.ajaxPrefilter(function(options, _, jqxhr){
+  if(options.url.indexOf(GHAPI) === 0){
+    var auth = btoa('lololololwut:omgwtfbbq0');
+    jqxhr.setRequestHeader('Authorization', 'Basic ' + auth);
+  }
+});
+
+// Ember stuff below
+
 App = Ember.Application.create();
 
 App.Router.map(function(){
@@ -6,4 +17,10 @@ App.Router.map(function(){
       this.resource('issue', {path: '/issues/:issue'});
     });
   });
+});
+
+App.UserRoute = Ember.Route.extend({
+  model: function(params){
+    return $.getJSON(GHAPI + '/users/' + params.username);
+  }
 });
