f8d03357d3b099342e3e2f438430aa6abfe2ee7c
Godfrey Chan <godfreykfc@gmail.com>
2014-05-14 13:21:51 -0700

    Step 2: define routes

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,9 @@
 App = Ember.Application.create();
+
+App.Router.map(function(){
+  this.resource('user', {path: '/:username'}, function(){
+    this.resource('repository', {path: '/:repository'}, function(){
+      this.resource('issue', {path: '/issues/:issue'});
+    });
+  });
+});
